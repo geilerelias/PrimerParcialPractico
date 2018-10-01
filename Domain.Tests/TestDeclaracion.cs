86c874241fb15e1de9788602da298dd6f69f16a2
geiler elias radillo <geilerelias@gmail.com>
2018-10-01 10:07:46 -0500

    este es el parcial

---
+++ Domain.Tests/TestDeclaracion.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Domain.Tests
+{
+    [TestFixture]
+    public class TestDeclaracion
+    {
+
+   
+        [Test]
+        public void TestDeclararMayorACeroSinEmplazamiento()
+        {
+            // TODO: Add your test code here
+            
+            var fecharegistro = DateTime.Parse("08/05/2018 8:34:01");
+            var valor = 0;
+            Declaracion declaracion = new Declaracion(valor,fecharegistro);
+            Assert.Equals("el valor de extemporaneidad es de $1.000.000", declaracion.LiquidarSancion());
+        }
+
+        [Test]
+        public void ValorMayorACeroConEmplazamiento()
+        {
+            var fecharegistro = DateTime.Parse("08/05/2018 8:34:01");
+            var valor = 0;
+            Declaracion declaracion = new Declaracion(valor, fecharegistro);
+            Assert.Equals("", declaracion.LiquidarSancion());
+        }
+
+        [Test]
+        public void ValorADeclararCeroSinEmplazamiento()
+        {
+            var fecharegistro = DateTime.Parse("08/05/2018 8:34:01");
+            var valor = 0;
+            Declaracion declaracion = new Declaracion(valor, fecharegistro);
+            Assert.Equals("", declaracion.LiquidarSancion());
+        }
+
+        [Test]
+        public void ValorADeclararCeroConEmplazamiento()
+        {
+            var fecharegistro = DateTime.Parse("08/05/2018 8:34:01");
+            var valor = 0;
+            Declaracion declaracion = new Declaracion(valor, fecharegistro);
+            Assert.Equals("", declaracion.LiquidarSancion());
+        }
+
+    }
+}
---
+++ Domain/Declaracion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Declaracion
+    {
+        decimal Valor { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Declaracion(int valor, DateTime Fecha)
+        {
+            Valor = valor;
+            this.Fecha = Fecha;
+        }
+
+   
+        public string  LiquidarSancion()
+        {
+            return "";
+        }
+    }
+}
